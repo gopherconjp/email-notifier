3cfa8bea3043ff9a1f28c461088b2577add25b61
Takuto Nagami <logica0419@gmail.com>
2026-08-30 23:16:55 +0900

    enable minifier
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  build: {
+    minify: "oxc",
+  },
   plugins: [cloudflare()],
 });
